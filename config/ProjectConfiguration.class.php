59f07a5ab2f21836d0f7ec6847a0c467bc414eaa
Sinpei Araki <speedsolve@gmail.com>
2012-10-05 07:51:03 +0900

    インデント修正

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -7,24 +7,24 @@ class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-     $this->enablePlugins('sfDoctrinePlugin');
+    $this->enablePlugins('sfDoctrinePlugin');
   }
 
   public function configureDoctrine(Doctrine_Manager $manager)
   {
-      // 設定の配列を追加して、利用するmemcachedサーバを追加できます
-      $servers = array(
-          array(
-              'host' => 'localhost',
-              'port' => 11211,
-              'persistent' => true),
-          );
-      $cacheDriver = new Doctrine_Cache_Memcache(array(
-          'servers' => $servers,
-          'compression' => false));
-      $manager->setAttribute(Doctrine_Core::ATTR_QUERY_CACHE, $cacheDriver);
-      $manager->setAttribute(Doctrine_Core::ATTR_QUERY_CACHE_LIFESPAN, 3600);
-      $manager->setAttribute(Doctrine_Core::ATTR_RESULT_CACHE, $cacheDriver);
-      $manager->setAttribute(Doctrine_Core::ATTR_RESULT_CACHE_LIFESPAN, 3600);
+    // 設定の配列を追加して、利用するmemcachedサーバを追加できます
+    $servers = array(
+      array(
+        'host' => 'localhost',
+        'port' => 11211,
+        'persistent' => true),
+      );
+    $cacheDriver = new Doctrine_Cache_Memcache(array(
+      'servers' => $servers,
+      'compression' => false));
+    $manager->setAttribute(Doctrine_Core::ATTR_QUERY_CACHE, $cacheDriver);
+    $manager->setAttribute(Doctrine_Core::ATTR_QUERY_CACHE_LIFESPAN, 3600);
+    $manager->setAttribute(Doctrine_Core::ATTR_RESULT_CACHE, $cacheDriver);
+    $manager->setAttribute(Doctrine_Core::ATTR_RESULT_CACHE_LIFESPAN, 3600);
   }
 }
