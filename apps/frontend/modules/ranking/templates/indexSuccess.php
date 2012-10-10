9b412fdf34182013c74195f1271d5c3937917bb4
Sinpei Araki <speedsolve@gmail.com>
2012-10-10 21:45:02 +0900

    Web App用に調整した

---
+++ apps/frontend/modules/ranking/templates/indexSuccess.php
@@ -0,0 +1,67 @@
+<div data-role="page" id="page1">
+    <div data-theme="a" data-role="header">
+        <h3>
+            Ranking
+        </h3>
+    </div>
+    <div data-role="content">
+        <form action="" method="POST">
+            <div data-role="fieldcontain">
+                <label for="selectmenu1">
+                </label>
+                <select name="" data-theme="a">
+                    <option value="">
+                        Event
+                    </option>
+                </select>
+            </div>
+            <div data-role="fieldcontain">
+                <label for="selectmenu2">
+                </label>
+                <select name="" data-theme="a">
+                    <option value="">
+                        Region
+                    </option>
+                </select>
+            </div>
+            <div data-role="fieldcontain">
+                <label for="selectmenu3">
+                </label>
+                <select name="" data-theme="a">
+                    <option value="">
+                        Years
+                    </option>
+                </select>
+            </div>
+            <div data-role="fieldcontain">
+                <label for="selectmenu4">
+                </label>
+                <select name="" data-theme="a">
+                    <option value="">
+                        Gender
+                    </option>
+                </select>
+            </div>
+            <div data-role="fieldcontain">
+                <fieldset data-role="controlgroup">
+                    <label for="slider1">
+                    </label>
+                    <input type="range" name="slider" value="100" min="0" max="1000" data-highlight="true" data-theme="a" data-track-theme="a">
+                </fieldset>
+            </div>
+            <div data-role="fieldcontain">
+                <fieldset data-role="controlgroup" data-type="horizontal">
+                    <input id="radio1" name="" value="" type="radio" data-theme="a" checked>
+                    <label for="radio1">
+                        Person
+                    </label>
+                    <input id="radio2" name="" value="" type="radio" data-theme="a">
+                    <label for="radio2">
+                        Result
+                    </label>
+                </fieldset>
+            </div>
+            <input type="submit" data-theme="a" data-icon="check" data-iconpos="left" value="Submit">
+        </form>
+    </div>
+</div>
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,14 +1,20 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <?php include_http_metas() ?>
-    <?php include_metas() ?>
-    <?php include_title() ?>
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <?php include_stylesheets() ?>
-    <?php include_javascripts() ?>
-  </head>
-  <body>
-    <?php echo $sf_content ?>
-  </body>
+<!DOCTYPE html>
+<html xml:lang="en" lang="ja">
+    <head>
+        <meta name="viewport" content="initial-scale=1.0">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="white">
+        <link rel="apple-touch-icon" href="images/ios_icon.png"/>
+        <link rel="apple-touch-startup-image" href="images/ios_startup.png" />
+        <?php include_title() ?>
+        <?php include_metas() ?>
+        <?php use_javascript('http://code.jquery.com/jquery-1.8.2.min.js') ?>
+        <?php use_javascript('http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js') ?>
+        <?php include_javascripts() ?>
+        <?php use_stylesheet('http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css') ?>
+        <?php include_stylesheets() ?>
+    </head>
+    <body>
+        <?php echo $sf_content ?>
+    </body>
 </html>
