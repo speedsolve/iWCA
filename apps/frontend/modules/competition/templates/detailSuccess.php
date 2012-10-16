4ffb8c33ee56713c37276b6f748680911b7a5dd8
Sinpei Araki <speedsolve@gmail.com>
2012-10-16 14:21:16 +0900

    戻りボタン追加

--- apps/frontend/modules/competition/templates/detailSuccess.php
+++ apps/frontend/modules/competition/templates/detailSuccess.php
@@ -1,4 +1,4 @@
-<div id="page" data-role="page" data-theme="a">
+<div id="page" data-role="page"  data-add-back-btn=”true” data-theme="a">
     <?php include_partial('global/header', array('title' => 'Competition')) ?>
     <div data-role="content">
     </div>
