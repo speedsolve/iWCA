cf7f3b4388792fe39ed113a7fdfc83a26bc10d6c
Sinpei Araki <speedsolve@gmail.com>
2012-10-18 00:08:37 +0900

    Person リンク追加

--- apps/frontend/templates/_history.php
+++ apps/frontend/templates/_history.php
@@ -2,21 +2,25 @@
     <?php foreach ($histories[$event] as $history): ?>
         <?php if ($history['regionalsinglerecord']): ?>
             <li>
-                <span class="event-name"><?php echo $value['cellname'] ?></span><br />
-                <?php echo $history['best'] ?><br />
-                <span class="competition-name"><?php echo $history['competitionname'] ?>&nbsp;&nbsp;<?php echo $history['roundid'] ?></span><br />
+                <a href="<?php echo url_for('competition/detail?competitionId='.$history['competitionid']) ?>" class="ui-link-inherit">
+                    <span class="event-name"><?php echo $value['cellname'] ?></span><br />
+                    <?php echo $history['best'] ?><br />
+                    <span class="competition-name"><?php echo $history['competitionname'] ?>&nbsp;&nbsp;<?php echo $history['roundid'] ?></span><br />
+                </a>
             </li>
         <?php endif ?>
         <?php if ($history['regionalaveragerecord']): ?>
             <li>
-                <span class="event-name"><?php echo $value['cellname'] ?></span><br />
-                <?php echo $history['average'] ?><br />
-                <span class="subrecord">
-                    <?php foreach($history['subrecord'] as $subrecord):?>
-                        <?php echo $subrecord ?>&nbsp;
-                    <?php endforeach; ?>
-                </span><br />
-                <span class="competition-name"><?php echo $history['competitionname'] ?>&nbsp;&nbsp;<?php echo $history['roundid'] ?></span><br />
+                <a href="<?php echo url_for('competition/detail?competitionId='.$history['competitionid']) ?>" class="ui-link-inherit">
+                    <span class="event-name"><?php echo $value['cellname'] ?></span><br />
+                    <?php echo $history['average'] ?><br />
+                    <span class="subrecord">
+                        <?php foreach($history['subrecord'] as $subrecord):?>
+                            <?php echo $subrecord ?>&nbsp;
+                        <?php endforeach; ?>
+                    </span><br />
+                    <span class="competition-name"><?php echo $history['competitionname'] ?>&nbsp;&nbsp;<?php echo $history['roundid'] ?></span><br />
+                </a>
             </li>
         <?php endif ?>
     <?php endforeach ?>
