32358d93306e6e7a749fc4542a9a609d5dab5f67
Sinpei Araki <speedsolve@gmail.com>
2014-03-13 21:24:17 +0900

    Google Adsense導入

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,4 +1,15 @@
 <div data-role="footer" data-id="footer" data-position="fixed" data-tap-toggle="false">
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- iWCA -->
+    <ins class="adsbygoogle"
+         style="display:inline-block;width:320px;height:50px"
+         data-ad-client="ca-pub-3051896224981468"
+         data-ad-slot="5369636483"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+
     <div data-role="navbar">
         <ul>
           <li><?php echo link_to('Ranking', 'ranking/index',     array('class' => $ranking_class)) ?></li>
