84578025d1adfd5e30e23859c06c9803932eb052
Sinpei Araki <speedsolve@gmail.com>
2014-06-15 19:42:37 +0900

    広告をAjaxとコンフリクトを起こさないように同期バージョンにした

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,4 +1,15 @@
 <div data-role="footer" data-id="footer" data-position="fixed" data-tap-toggle="false">
+
+    <script type="text/javascript"><!--
+        google_ad_client = "ca-pub-3051896224981468";
+        /* iWCA */
+        google_ad_slot = "5369636483";
+        google_ad_width = 320;
+        google_ad_height = 50;
+        //-->
+    </script>
+    <script type="text/javascript" src="//pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+
     <div data-role="navbar">
         <ul>
           <li><?php echo link_to('Ranking', 'ranking/index',     array('class' => $ranking_class)) ?></li>
