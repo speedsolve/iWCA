4f93c5b06f180e50cbadef790c08ee8077686935
Sinpei Araki <speedsolve@gmail.com>
2014-10-26 01:23:20 +0900

    地図用の緯度計算を100万で割るようにした

--- lib/service/CompetitionsService.class.php
+++ lib/service/CompetitionsService.class.php
@@ -32,24 +32,7 @@ class CompetitionsService
    */
   public static function getChangeCoordinates($point)
   {
-    if (strlen($point) == 7) {
-      $pre   = substr($point, 0, 1);
-      $after = substr($point, 1, 6);
-      $point = $pre.'.'.$after;
-    } else if (strlen($point) == 8) {
-      $pre   = substr($point, 0, 2);
-      $after = substr($point, 2, 6);
-      $point = $pre.'.'.$after;
-    } else if (strlen($point) == 9) {
-      $pre   = substr($point, 0, 3);
-      $after = substr($point, 3, 6);
-      $point = $pre.'.'.$after;
-    } else if (strlen($point) == 10) {
-      $pre   = substr($point, 0, 4);
-      $after = substr($point, 4, 6);
-      $point = $pre.'.'.$after;
-    }
-    return $point;
+    return $point / 1000000;
   }
 
   /**
