b1b0f9cec8cd1a797e566798467b8ba666e46e78
Sinpei Araki <speedsolve@gmail.com>
2014-11-19 02:22:07 +0900

    liveページ、戻りボタン削除

--- apps/frontend/modules/live/templates/listSuccess.php
+++ apps/frontend/modules/live/templates/listSuccess.php
@@ -1,4 +1,4 @@
-<div id="page" data-role="page" data-add-back-btn=”true” data-theme="a">
+<div id="page" data-role="page" data-theme="a">
     <?php include_partial('global/header', array('title' => 'Live')) ?>
     <div data-role="content">
         <ul data-role="listview" data-theme="a" data-divider-theme="a">
