b2a7975d4fb651ff8cbf5fd1c79581cb114b82c8
Sinpei Araki <speedsolve@gmail.com>
2015-10-21 01:46:06 +0900

    リンク漏れ修正

--- apps/frontend/modules/ranking/templates/singleSuccess.php
+++ apps/frontend/modules/ranking/templates/singleSuccess.php
@@ -19,17 +19,19 @@
                             <?php endif ?>
                         </a>
                     <?php elseif ($type == 'rank'): ?>
-                        <?php echo $result['rank'] ?>.&nbsp;<?php echo image_tag('flag/' . $result['personcountryid'] . '.png', array('class' => 'ui-li-icon', 'style' => 'max-width:28px;max-height:28px;top:5px;left:5px;')) ?>&nbsp;<?php echo $result['personname'] ?><br />
-                        <?php echo $result['record'] ?>&nbsp;<span class="ranking-prize-point">pt</span><br />
-                        <?php foreach (sfConfig::get('app_event_id') as $event => $target): ?>
-                            <?php if (!in_array($event, sfConfig::get('app_event_abolition'))): ?>
-                                <?php if ($result[$event] == $counts[$event] + 1): ?>
-                                    <span class="rank-event-title"><?php echo $event ?></span>&nbsp;<span class="rank-event-dead-rank"><?php echo $result[$event] ?></span>
-                                <?php else: ?>
-                                    <span class="rank-event-title"><?php echo $event ?></span>&nbsp;<span class="rank-event-rank"><?php echo $result[$event] ?></span>
+                        <a href="<?php echo url_for('person/detail?id='.$result['personid']) ?>" class="ui-link-inherit">
+                            <?php echo $result['rank'] ?>.&nbsp;<?php echo image_tag('flag/' . $result['personcountryid'] . '.png', array('class' => 'ui-li-icon', 'style' => 'max-width:28px;max-height:28px;top:5px;left:5px;')) ?>&nbsp;<?php echo $result['personname'] ?><br />
+                            <?php echo $result['record'] ?>&nbsp;<span class="ranking-prize-point">pt</span><br />
+                            <?php foreach (sfConfig::get('app_event_id') as $event => $target): ?>
+                                <?php if (!in_array($event, sfConfig::get('app_event_abolition'))): ?>
+                                    <?php if ($result[$event] == $counts[$event] + 1): ?>
+                                        <span class="rank-event-title"><?php echo $event ?></span>&nbsp;<span class="rank-event-dead-rank"><?php echo $result[$event] ?></span>
+                                    <?php else: ?>
+                                        <span class="rank-event-title"><?php echo $event ?></span>&nbsp;<span class="rank-event-rank"><?php echo $result[$event] ?></span>
+                                    <?php endif; ?>
                                 <?php endif; ?>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
+                            <?php endforeach; ?>
+                        </a>
                     <?php else: ?>
                         <a href="<?php echo url_for('person/detail?id='.$result['personid']) ?>" class="ui-link-inherit">
                             <?php echo $result['rank'] ?>.&nbsp;<?php echo image_tag('flag/' . $result['personcountryid'] . '.png', array('class' => 'ui-li-icon', 'style' => 'max-width:28px;max-height:28px;top:5px;left:5px;')) ?>&nbsp;<?php echo $result['personname'] ?><br />
